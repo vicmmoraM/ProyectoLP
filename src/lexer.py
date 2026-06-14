1c7af13c593d6df63ee2bf1fd3dcbba251edddd1
vicmmoraM <vicmmora@espol.edu.ec>
2026-06-14 01:05:35 -0500

    feat: estructura base lexer.py

---
+++ src/lexer.py
@@ -0,0 +1,15 @@
+import ply.lex as lex
+#no se olviden de instalar ply -> comando par instalar: pip install ply
+#Listado con tokens
+
+tokens = [
+    # Victor Morales - Inicio
+
+    # Victor Morales - Fin
+    # Jose Adrian - Inicio
+
+    # Jose Adrian - Fin
+    # Andres Saltos - Inicio
+
+    # Andres Saltos - Fin
+]
